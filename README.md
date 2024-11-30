ae9c90467c1f713f5c92c8f2004d853452524a18
kenjutzue <grefielkent@gmail.com>
2024-11-30 22:04:23 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Glassmorphism vs Neumorphism Calculator | CSS & JS(VanillaTilt)
+
+A calculator of using html, css , and javascript
---
+++ src/script.js
@@ -0,0 +1,6 @@
+VanillaTilt.init(document.querySelector(".content"), {
+		max: 15,
+		speed: 400,
+		glare: true,
+		"max-glare": 0.2,
+	});
